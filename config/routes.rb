4bb8b876ddb569e70b251410fd3a70f92426086c
Guillaume Lenzinger <glenzinger@gmail.com>
2020-01-28 21:30:32 -0500

    in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Lire toutes les tasks
+  #get 'tasks', to: 'tasks#index'
+  # Lire une task
+  #get 'tasks/:id', to: 'tasks#show'
+  # Creer une task
+  #post 'tasks', to: 'tasks#create'
+  #get 'tasks/new', to: 'tasks#new'
+  # Updater task
+  #get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  #patch 'tasks/:id', to: 'tasks#update'
+  # Supprimer une task
+  #delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
